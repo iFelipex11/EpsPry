45b4e236c95b4fa29e0b3575b4d0bc513db5adf6
iFelipex <felipeap1011@gmail.com>
2025-09-10 17:25:26 -0500

    Nuevos documentosv1

--- src/pfx/App.java
+++ src/pfx/App.java
@@ -5,13 +5,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
   @Override
   public void start(Stage stage) throws Exception {
+    // Cargar el archivo FXML
     Parent root = FXMLLoader.load(getClass().getResource("Start.fxml"));
-    stage.setTitle("Clínica - Inicio");
+    
+    // Establecer el título de la ventana
+    stage.setTitle("Clininova");
+
+    // Establecer el ícono de la ventana
+    Image icon = new Image("/Imgs/Logo.jpeg"); // Reemplaza con la ruta correcta a tu imagen
+    stage.getIcons().add(icon);
+
+    // Configurar la escena y mostrar la ventana
     stage.setScene(new Scene(root));
     stage.show();
   }
@@ -22,5 +32,7 @@ public class App extends Application {
     super.stop();
   }
 
-  public static void main(String[] args) { launch(args); }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
